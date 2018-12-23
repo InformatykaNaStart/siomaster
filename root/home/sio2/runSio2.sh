076f9a38a7312cf257afa70eb5112b6e8552d7c2
Mateusz Żółtak <zozlak@zozlak.org>
2018-12-23 13:12:01 +0100

    Fixed sample data timestamps

--- root/home/sio2/runSio2.sh
+++ root/home/sio2/runSio2.sh
@@ -1,6 +1,6 @@
 if [ "$INIT_DB" == "on" ]; then
     echo "Filling in the sio2 database with a sample data"
-    cp -R /home/sio2/sample_files /home/sio2/deployment/media/files
+    tar -x -C /home/sio2/deployment/media/files -f /home/sio2/sample_files.tar
     sleep 5 # give postgresql time to start
     psql -f /home/sio2/sio2.sql sio2
 fi
